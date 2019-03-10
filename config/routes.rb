4c0781689e458aefe24d97f592cd8a6644815a8b
yut0201 <yuta.yamauchi@jibunstyle.com>
2019-03-11 08:41:03 +0900

    userコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/new'
+  get 'user/edit'
+  get 'user/delete'
   get '/' => "home#top"
   get '/about' => "home#about"
 
