77ae6ae04fefe69395a807a6ee9d243fbf0d03e7
yut0201 <yuta.yamauchi@jibunstyle.com>
2019-08-01 12:24:09 +0900

    パスとスキーマの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_145608) do
+ActiveRecord::Schema.define(version: 2019_07_29_025300) do
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_03_26_145608) do
     t.string "display_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "Event_id"
+    t.index ["Event_id"], name: "index_presentations_on_Event_id"
   end
 
   create_table "users", force: :cascade do |t|
