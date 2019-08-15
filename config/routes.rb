ac6b197e66bb282a0323fc156f0ef9439f2eb383
yut0201 <yuta.yamauchi@jibunstyle.com>
2019-08-15 23:28:52 +0900

    ルーティングの修正(EventsとPresentationsをネスト)

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   get    '/home',       to: 'home#top'
   get    '/about',      to: 'home#about'
 
-  resources :events, param: :event_name
+  resources :events, param: :event_name, shallow: true do
+    resources :presentations
+  end
 
   resources :users
 
   resources :sessions
 
-  resources :presentations
+  # resources :presentations, param: :event_name
 
 end
