6c954f055cc3bc553ccb0909eb9e2f4202853812
yut0201 <yuta.yamauchi@jibunstyle.com>
2019-10-11 11:41:11 +0900

    細かい修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_122236) do
+ActiveRecord::Schema.define(version: 2019_09_24_155758) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "presentation_id"
+    t.string "sentence"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
